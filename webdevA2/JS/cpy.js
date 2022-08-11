395e66b07a53cc47e4ea03f4e81bcda96587ca76
Laerxhte <55490637+Laerxhte@users.noreply.github.com>
2022-08-12 07:46:12 +0800

    Update 1

--- webdevA2/JS/cpy.js
+++ webdevA2/JS/cpy.js
@@ -1,16 +1,16 @@
 // let d = new Date();
 // window.alert("Today's date is " + d);
 // document.write("hey hey what"); 
-function writeHello(selector, text){
-    var introBox = document.querySelector(selector);
+// function writeHello(selector, text){
+//     var introBox = document.querySelector(selector);
 
-    var pElement = document.createElement("h1");
+//     var pElement = document.createElement("h1");
 
-    var txt = document.createTextNode(text);
-    pElement.appendChild(txt);
+//     var txt = document.createTextNode(text);
+//     pElement.appendChild(txt);
 
-    introBox.appendChild(pElement);
-}
+//     introBox.appendChild(pElement);
+// }
 
-writeHello(".box2", "IT'S ALL ABOUT MOTORCYCLES");
+// writeHello(".box2", "IT'S ALL ABOUT MOTORCYCLES");
 
